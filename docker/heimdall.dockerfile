bc684ba12c70d653f892c09e6c5ae11bbd51578f
AlbertHuang <r.albert.huang@gmail.com>
2022-01-19 10:45:40 +0800

    fix: heimdall start

--- docker/heimdall.dockerfile
+++ docker/heimdall.dockerfile
@@ -1,9 +1,5 @@
 FROM maticnetwork/heimdall:v0.2.4
 
-RUN apt-get install -y systemctl
-
-COPY ./docker/heimdalld.service /etc/systemd/system/heimdalld.service
-
 COPY ./docker/heimdall.start.sh /usr/local/bin/heimdall.start.sh
 
 ENTRYPOINT ["bash", "/usr/local/bin/heimdall.start.sh"]
--- docker/heimdall.start.sh
+++ docker/heimdall.start.sh
@@ -5,22 +5,25 @@ set -o pipefail
 set -e
 
 init() {
-    if [ -f $HOME/heimdall_data/initialized ]; then
-        return 0;
+    if [ ! -f "$HOME/heimdall_data/initialized" ]; then
+        mkdir -p $HOME/heimdall_data
+        heimdalld init --home $HOME/heimdall_data
+        wget https://raw.githubusercontent.com/maticnetwork/launch/master/mainnet-v1/sentry/sentry/heimdall/config/genesis.json
+        cp -rf genesis.json $HOME/heimdall_data/config/genesis.json
+        # edit $HOME/heimdall_data/config/config.toml
+        sed -i '/moniker/c\moniker = "MelandPolygonNode"' $HOME/heimdall_data/config/config.toml
+        sed -i '/seeds/c\seeds = "f4f605d60b8ffaaf15240564e58a81103510631c@159.203.9.164:26656,4fb1bc820088764a564d4f66bba1963d47d82329@44.232.55.71:26656"' $HOME/heimdall_data/config/config.toml
+        # wget -c https://matic-blockchain-snapshots.s3-accelerate.amazonaws.com/matic-mainnet/heimdall-snapshot-2022-01-17.tar.gz -O - | tar -xz -C $HOME/heimdall_data/data
+        touch $HOME/heimdall_data/initialized
     fi
-    mkdir -p $HOME/heimdall_data
-    heimdalld init --home $HOME/heimdall_data
-    wget https://raw.githubusercontent.com/maticnetwork/launch/master/mainnet-v1/sentry/sentry/heimdall/config/genesis.json
-    cp -rf genesis.json $HOME/heimdall_data/config/genesis.json
-    # edit $HOME/heimdall_data/config/config.toml
-    sed -i '/moniker/c\moniker = "MelandPolygonNode"' $HOME/heimdall_data/config/config.toml
-    sed -i '/seeds/c\seeds = "f4f605d60b8ffaaf15240564e58a81103510631c@159.203.9.164:26656,4fb1bc820088764a564d4f66bba1963d47d82329@44.232.55.71:26656"' $HOME/heimdall_data/config/config.toml
-    wget -c https://matic-blockchain-snapshots.s3-accelerate.amazonaws.com/matic-mainnet/heimdall-snapshot-2022-01-17.tar.gz -O - | tar -xz -C $HOME/heimdall_data/data
-    touch $HOME/heimdall_data/initialized
 }
 
 init
 
-systemctl start heimdalld
+echo "start heimdall";
+
+nohup heimdalld start --home=/root/heimdall_data &
+
+echo "start rest-server";
 
 heimdalld rest-server --home $HOME/heimdall_data --chain-id=137
